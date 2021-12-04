50c3554334e992df57ab58af583b9453d557d1d2
Onur Tekin <onurtekin314@gmail.com>
2021-12-04 13:11:06 +0300

    end of section 11

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
-import { hostname } from "os";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { Activity } from "../models/activity";
